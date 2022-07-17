e6b9b3c28a24e365fb66b70c3320a39557291e8f
mahmoodaltayeb <109466536+mahmoodaltayeb@users.noreply.github.com>
2022-07-17 16:41:57 +0300

    update code

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -6,7 +6,6 @@
         categories 
         @endsection
         @section('body')
-
         <div class="container-fluid py-4">
           <div class="row">
             <div class="col-12">
@@ -17,8 +16,6 @@
                   </div>
                 </div>
                 <div class="card-body px-0 pb-2">
-
-
                   <div class="row">  
                     <div class="form-border rounded mt-2">
                      <form  role="form" action="{{route('categories.store')}}" method="Post" class="mt-4 m-5">
@@ -31,20 +28,20 @@
                         </div>
                       </div>
                     </div>
-        </div>
-        <div class="text-center mb-3">
-          <button type="submit  " class="btn btn-lg  btn-lg  mt-4 mb-0 bg-gradient-dark">Submit</button>
+                  </div>
+                  <div class="text-center mb-3">
+                    <button type="submit  " class="btn btn-lg  btn-lg  mt-4 mb-0 bg-gradient-dark">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-    </form>
+    </div>
   </div>
-</div>
-</div>
-</div>
-</div>
-</div>
 
 
 
 
-@endsection
+  @endsection
